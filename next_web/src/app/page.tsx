ed62d753d24d117804de1af4e9598394e7e8344c
gonzalo <gonzalo.larroya-martinez@epitech.eu>
2024-06-18 20:46:37 +0200

    [add] Svg Map

--- next_web/src/app/page.tsx
+++ next_web/src/app/page.tsx
@@ -1,7 +1,7 @@
 // Main Landing Page "/" with dual maps
 'use client'
 import { useEffect, useState } from "react";
-import Map1 from "@/components/Map";
+import Map from "@/components/Map";
 import 'ol-ext/dist/ol-ext.css';
 import { Button } from "@/components/ui/button"
 
@@ -12,8 +12,9 @@ function DualMap() {
       <header className="flex h-16 w-full gap-4 items-center justify-center border-b">
         <Button onClick={() => console.log('clicked')}>Click me</Button>
       </header>
-        <Map1 setMap1Object={setMap1Object} />
-      {/* Uncomment the following line when Map1 is ready to be used */}
+        {/* <Map1 setMap1Object={setMap1Object} /> */}
+        <Map />
+      {/* Uncomment the following line when Map1 is ready to be used*/}
     </>
   );
 }
